e54e33553c1009656b91700249f4bf5bea0c0121
Jack <104746198+mortonjack@users.noreply.github.com>
2022-12-13 18:06:07 +1030

    day 13

---
+++ 2022/day13/puzzle1.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using std::vector;
+using std::cout; using std::endl;
+using std::string; using std::ifstream;
+
+#define MAX_LENGTH 250 // max length of lines
+
+class Pair {
+    public:
+        bool integer; /* List or Int */
+        int val; /* Value (if integer) */
+        vector<Pair*> list; /* List of elements (could be integers or lists) */
+
+        // Constructor
+        Pair(int x) {
+            integer = true;
+            val = x;
+        }
+        
+        // Destructor
+        ~Pair() {
+            for (int i = 0; i < (int)list.size(); i++) {
+                delete list[i];
+            }
+        }
+
+        Pair(bool is_integer) {
+            integer = is_integer;
+            val = -1;
+        }
+
+        void insert(int x) {
+            if (integer) val = x;
+            else {
+                if (x == -1) list.push_back(new Pair(false)); /* Add new list to list */
+                else list.push_back(new Pair(x)); /* Add new int to list */
+            }
+        }
+};
+
+void print_pair(Pair* a) {
+    if (a->integer) cout << a->val << ",";
+    else {
+        cout << "[";
+        for (int i = 0; i < (int)a->list.size(); i++) print_pair(a->list[i]);
+        cout << "],";
+    }
+}
+
+int compare(Pair* left, Pair* right) {
+    /* Returns true if sorted correctly */
+
+    if (left->integer && right->integer) {
+        /* Both integers */
+        if (left->val > right->val) {
+            cout << "return -1, " << left->val << " > " << right->val << endl;
+            return -1;
+        }
+        else if (left->val < right->val) {
+            cout << "return 1, " << left->val << " < " << right->val << endl;
+            return 1;
+        }
+    } else if (!left->integer && !right->integer) {
+        /* Neither integers - compare each value of their lists */
+        for (int i = 0; i < (int)left->list.size(); i++) {
+            /* Return false if right runs out of items first */
+            if (!(i < (int)right->list.size())) {
+                cout << "return -1 right runs out of items first\n";
+                return -1;
+            }
+            /* Return false if unsorted (recursion) */
+            int c = compare(left->list[i], right->list[i]);
+            if (c != 0) return c;
+        }
+        /* Return 1 if right still has items */
+        if (left->list.size() != right->list.size()) {
+            cout << "return 1 left runs out of items first\n";
+            return 1;
+        }
+    } else if (left->integer) {
+        /* Only left integer */
+        if (right->list.size() == 0) {
+            cout << "Return -1 right has no integers\n";
+            return -1;
+        }
+        return compare(left, right->list[0]);
+    } else {
+        /* Only right integer */
+        if (left->list.size() == 0) {
+            cout << "Return 1 left has no integers\n";
+            return 1;
+        }
+        /* Only 1 element in list */
+        int comparison = compare(left->list[0], right);
+        if (left->list.size() == 1) {
+            return comparison;
+        }
+        /* Multiple element in list */
+        return comparison != 0 ? comparison : -1;
+    }
+
+    // No errors found, return 0
+    return 0;
+}
+
+void read(string& line, int& i, Pair * current_pair) {
+    // Read contents of line into pair
+    while (i < (int)line.length()) {
+        if (line[i] == '[') {
+            // New list
+            Pair* new_list = new Pair(false);
+            current_pair->list.push_back(new_list);
+            // Read contents of list into pair
+            read(line, ++i, new_list);
+        } else if (line[i] == ']') {
+            i++;
+            // End list
+            return;
+        } else if (line[i] == ',') {
+            i++;
+        } else {
+            // Append integer to back of list
+            int current = stoi(line.substr(i, i+2));
+            if (current > 9) i++;
+            current_pair->list.push_back(new Pair(current));
+            i++;
+        }
+    }
+}
+
+int main() {
+    ifstream file;
+    file.open("input.txt");
+
+    char thisLine[MAX_LENGTH];
+    string line;
+    file.getline(thisLine, MAX_LENGTH);
+    line.assign(thisLine);
+    int count = 0; // 0 = left, 1 = right; 2 = gappppp
+    int index = 0;
+
+    Pair * left;
+    Pair * right;
+
+    int sum = 0;
+
+    while (!file.eof()) {
+        if (count == 0) index++;
+
+        // Read line
+        Pair * current = new Pair(false);
+        int i = 1;
+        read(line, i, current);
+        if (count == 0) left = current;
+        else right = current;
+
+        // Compare pair
+        if (current == right) {
+/*            print_pair(left);
+            cout << endl;
+            print_pair(right);
+            cout << endl;
+  */        int comparative = compare(left, right);
+            if (comparative != -1) {
+                if (comparative == 0) cout << "Left side ran out of items, no conflicts found\n";
+                sum += index;
+                cout << "Index " << index << " sorted, total sum " << sum << endl;
+            }
+        }
+
+        // Prepare for next line
+        do {
+            file.getline(thisLine, MAX_LENGTH);
+            line.assign(thisLine);
+            count++;
+            count %= 3;
+        } while (count == 2);
+    }
+
+    cout << sum << endl;
+
+    file.close();
+    return 0;
+}
---
+++ 2022/day13/puzzle2.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <vector>
+using std::vector; using std::sort;
+using std::cout; using std::endl;
+using std::string; using std::ifstream;
+
+#define MAX_LENGTH 250 // max length of lines
+
+/* Be warned this is not at all memory safe */
+
+class Pair {
+    public:
+        bool integer; /* List or Int */
+        int val; /* Value (if integer) */
+        vector<Pair*> list; /* List of elements (could be integers or lists) */
+        bool divider;
+
+        // Constructor
+        Pair(int x) {
+            integer = true;
+            divider = false;
+            val = x;
+        }
+        
+        Pair(bool is_integer) {
+            divider = false;
+            integer = is_integer;
+            val = -1;
+        }
+
+        void insert(int x) {
+            if (integer) val = x;
+            else {
+                if (x == -1) list.push_back(new Pair(false)); /* Add new list to list */
+                else list.push_back(new Pair(x)); /* Add new int to list */
+            }
+        }
+};
+
+void print_pair(Pair* a) {
+    if (a->integer) cout << a->val << ",";
+    else {
+        cout << "[";
+        for (int i = 0; i < (int)a->list.size(); i++) print_pair(a->list[i]);
+        cout << "],";
+    }
+}
+
+int compare(Pair* left, Pair* right) {
+    cout << "New comparison\n";
+    if (left == nullptr ) cout << "AAAAAAA\n";
+    /* Returns true if sorted correctly */
+
+    if (left->integer && right->integer) {
+        cout << "Both ints\n";
+        /* Both integers */
+        if (left->val > right->val) {
+            cout << "return -1, " << left->val << " > " << right->val << endl;
+            return -1;
+        }
+        else if (left->val < right->val) {
+            cout << "return 1, " << left->val << " < " << right->val << endl;
+            return 1;
+        }
+    } else if (!left->integer && !right->integer) {
+        cout << "Neither ints\n";
+        /* Neither integers - compare each value of their lists */
+        for (int i = 0; i < (int)left->list.size(); i++) {
+            /* Return false if right runs out of items first */
+            if (!(i < (int)right->list.size())) {
+                cout << "return -1 right runs out of items first\n";
+                return -1;
+            }
+            /* Return false if unsorted (recursion) */
+            cout << "Compare->\n";
+            int c = compare(left->list[i], right->list[i]);
+            cout << "back\n";
+            if (c != 0) return c;
+        }
+        cout << "Outside of loop\n";
+        /* Return 1 if right still has items */
+        if (left->list.size() != right->list.size()) {
+            cout << "return 1 left runs out of items first\n";
+            return 1;
+        }
+    } else if (left->integer) {
+        cout << "Left int\n";
+        /* Only left integer */
+        if (right->list.size() == 0) {
+            cout << "Return -1 right has no integers\n";
+            return -1;
+        }
+        return compare(left, right->list[0]);
+    } else {
+        cout << "Right int\n";
+        /* Only right integer */
+        if (left->list.size() == 0) {
+            cout << "Return 1 left has no integers\n";
+            return 1;
+        }
+        /* Only 1 element in list */
+        int comparison = compare(left->list[0], right);
+        if (left->list.size() == 1) {
+            return comparison;
+        }
+        /* Multiple element in list */
+        return comparison != 0 ? comparison : -1;
+    }
+
+    // No errors found, return 0
+    return 0;
+}
+
+int base_compare(Pair* left, Pair* right) {
+    if (compare(left, right) != -1) return true;
+    return false;
+}
+
+void read(string& line, int& i, Pair * current_pair) {
+    // Read contents of line into pair
+    while (i < (int)line.length()) {
+        if (line[i] == '[') {
+            // New list
+            Pair* new_list = new Pair(false);
+            current_pair->list.push_back(new_list);
+            // Read contents of list into pair
+            read(line, ++i, new_list);
+        } else if (line[i] == ']') {
+            i++;
+            // End list
+            return;
+        } else if (line[i] == ',') {
+            i++;
+        } else {
+            // Append integer to back of list
+            int current = stoi(line.substr(i, i+2));
+            if (current > 9) i++;
+            current_pair->list.push_back(new Pair(current));
+            i++;
+        }
+    }
+}
+
+int main() {
+    ifstream file;
+    file.open("input.txt");
+
+    char thisLine[MAX_LENGTH];
+    string line;
+    file.getline(thisLine, MAX_LENGTH);
+    line.assign(thisLine);
+    int count = 0; // 0 = left, 1 = right; 2 = gappppp
+    int index = 0;
+
+    vector<Pair*> packets;
+
+    // Add divider packets
+    Pair* div1 = new Pair(false);
+    string div_1 = "[[2]]";
+    string div_2 = "[[6]]";
+    int i = 1;
+    read(div_1, i, div1);
+    packets.push_back(div1);
+    Pair* div2 = new Pair(false);
+    i = 1;
+    read(div_2, i, div2);
+    div1->divider = true;
+    div2->divider = true;
+    packets.push_back(div2);
+
+    while (!file.eof()) {
+        index++;
+
+        // Read line
+        Pair * current = new Pair(false);
+        int i = 1;
+        read(line, i, current);
+
+        // Append to vector
+        packets.push_back(current);
+
+        // Prepare for next line
+        do {
+            file.getline(thisLine, MAX_LENGTH);
+            line.assign(thisLine);
+            count++;
+            count %= 3;
+        } while (count == 2);
+    }
+
+    // Sort packets
+    cout << "Begin sort\n";
+    sort(packets.begin(), packets.end(), base_compare);
+
+    // Search for dividers
+    int decoder = 1;
+    cout << "Decoder search\n";
+    for (int i= 1; i <=(int) packets.size(); i++) {
+        if (packets[i-1]->divider) {
+            decoder *= i;
+            cout << "DECODER " << i << endl;
+        }
+    }
+    cout << decoder << endl;
+
+    file.close();
+    return 0;
+}
