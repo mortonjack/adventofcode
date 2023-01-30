c52203eb4d3166fec7fc7aa887ec58381cc882b9
Jack <104746198+mortonjack@users.noreply.github.com>
2023-01-30 16:31:44 +1030

    Puzzle 1 cleanup

--- 2022/day25/puzzle1.cpp
+++ 2022/day25/puzzle1.cpp
@@ -9,7 +9,7 @@ using std::string; using std::ifstream;
 
 #define MAX_LENGTH 30 /* max length of lines */
 
-void add_snafu_nums(vector<int>& total, string s, unordered_map<char, long long> snafu) {
+void add_snafu_nums(vector<int>& total, string s, unordered_map<char, int>& snafu) {
     /* First: Appropriately add things */
     for (int i = s.length()-1; i >= 0; i--) {
         total.at(s.length()-1-i) += snafu.at(s.at(i));
@@ -21,7 +21,7 @@ void add_snafu_nums(vector<int>& total, string s, unordered_map<char, long long>
             if (i+1 >= (int)total.size()) total.push_back(0);
             total.at(i+1)--;
         }
-        while (total.at(i) > 2) {
+        if (total.at(i) > 2) {
             total.at(i) -= 5;
             if (i+1 >= (int)total.size()) total.push_back(1);
             else total.at(i+1)++;
@@ -29,7 +29,7 @@ void add_snafu_nums(vector<int>& total, string s, unordered_map<char, long long>
     }
 }
 
-string vec_to_snafu(vector<int>& total, unordered_map<int, char> decimal) {
+string vec_to_snafu(vector<int>& total, unordered_map<int, char>& decimal) {
     bool first = false;
     string res;
     for (int i = (int)total.size()-1; i >= 0; i--) {
@@ -52,7 +52,7 @@ int main() {
     file.getline(thisLine, MAX_LENGTH);
     line.assign(thisLine);
 
-    unordered_map<char, long long> snafu;
+    unordered_map<char, int> snafu;
     snafu['='] = -2;
     snafu['-'] = -1;
     snafu['0'] = 0;
