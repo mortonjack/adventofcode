b474b1981e58e470b68c66cb7fb69d9f0cd5fa8a
mortonjack <morton.jack@protonmail.com>
2023-12-01 18:11:19 +0800

    AoC 2023 day 1 part 1

---
+++ 2023/day1/part1.ml
@@ -0,0 +1,21 @@
+open Base
+open Stdio
+
+let first_digit str = String.find str ~f:(fun c -> Char.(c >= '0' && c <= '9'))
+
+let calibration str = 
+  let digit1 = match first_digit str with
+  | None -> "0"
+  | Some x -> String.of_char x
+in
+  let digit2 = match first_digit (String.rev str) with
+  | None -> "0"
+  | Some x -> String.of_char x
+in
+  Int.of_string (digit1 ^ digit2)
+
+let () =
+let readings = In_channel.input_lines stdin in
+let cal = List.fold readings ~init:0 ~f:(fun acc str -> acc + (calibration str)) in
+Out_channel.output_string stdout (String.concat [(Int.to_string cal); "\n"]);
+Out_channel.flush stdout
