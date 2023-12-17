6271bd1f97006afd93df71640916f0e5bc3b6e89
mortonjack <morton.jack@protonmail.com>
2023-12-17 14:54:15 +0800

    part 2

--- 2023/day17/part2.ml
+++ 2023/day17/part2.ml
@@ -3,27 +3,29 @@ open Stdio
 open! Containers.Heap
 
 type direction = Up | Down | Left | Right
-let new_movecount dir1 dir2 moves =
+let new_movecount dir1 dir2 moves need_moves =
   match dir1, dir2 with
-  | Up, Up | Down, Down | Left, Left | Right, Right -> moves - 1
-  | _ -> 3
+  | Up, Up | Down, Down | Left, Left | Right, Right -> moves - 1, need_moves - 1
+  | _ -> 10, 4
 
 module Pos = struct
   type t = {
     r : int;
     c : int;
     moves : int;
+    need_moves : int;
     heat_lost : int;
     dir : direction
   } [@@deriving fields ~getters]
 
   let in_dir pos dir = 
-    let moves = new_movecount dir pos.dir pos.moves in
+    let moves, need_moves = new_movecount dir pos.dir pos.moves pos.need_moves in
+    let pos = { pos with moves; need_moves; dir } in
     match dir with
-    | Up -> { pos with r = pos.r - 1; moves; dir }
-    | Down -> { pos with r = pos.r + 1; moves; dir }
-    | Left -> { pos with c = pos.c - 1; moves; dir }
-    | Right -> { pos with c = pos.c + 1; moves; dir }
+    | Up -> { pos with r = pos.r - 1 }
+    | Down -> { pos with r = pos.r + 1 }
+    | Left -> { pos with c = pos.c - 1 }
+    | Right -> { pos with c = pos.c + 1 }
 
   let leq first second =
     first.heat_lost <= second.heat_lost
@@ -31,7 +33,8 @@ module Pos = struct
   let add_heat pos ~map =
     { pos with heat_lost = pos.heat_lost + map.(pos.r).(pos.c)}
 
-  let create ~r ~c ~moves ~heat_lost ~dir = {r; c; moves; heat_lost; dir}
+  let create ~r ~c ~moves ~need_moves ~heat_lost ~dir = 
+    {r; c; moves; need_moves; heat_lost; dir}
 
 end
 
@@ -43,9 +46,11 @@ end)
 let new_valid_dirs map pos = 
   let dir = Pos.dir pos in
   let straight = Pos.in_dir pos dir in
-  let move_list = match dir with
-  | Up | Down -> Pos.in_dir pos Left :: Pos.in_dir pos Right :: straight :: []
-  | Left | Right -> Pos.in_dir pos Up :: Pos.in_dir pos Down :: straight :: []
+  let move_list = 
+    if Pos.need_moves pos <= 1 then match dir with
+    | Up | Down -> Pos.in_dir pos Left :: Pos.in_dir pos Right :: straight :: []
+    | Left | Right -> Pos.in_dir pos Up :: Pos.in_dir pos Down :: straight :: []
+    else [straight]
   in
   List.filter move_list ~f:(fun pos ->
     let r = Pos.r pos in
@@ -72,7 +77,8 @@ let make_seen pos seen =
 
 let rec dijkstras map heap seen (end_r, end_c as end_pos) = 
   let (heap, pos) = PosHeap.take_exn heap in
-  if Pos.r pos = end_r && Pos.c pos = end_c then Pos.heat_lost pos
+  if Pos.r pos = end_r && Pos.c pos = end_c && Pos.need_moves pos <= 1
+  then Pos.heat_lost pos
   else if is_seen pos seen then dijkstras map heap seen end_pos else (
     make_seen pos seen;
     let heap = PosHeap.add_list heap (new_valid_dirs map pos) in
@@ -80,8 +86,8 @@ let rec dijkstras map heap seen (end_r, end_c as end_pos) =
   )
 
 let find_shortest_path map = 
-  let start1 = Pos.create ~r:0 ~c:0 ~moves:0 ~heat_lost:0 ~dir:Down in
-  let start2 = Pos.create ~r:0 ~c:0 ~moves:0 ~heat_lost:0 ~dir:Right in
+  let start1 = Pos.create ~r:0 ~c:0 ~moves:0 ~need_moves:0 ~heat_lost:0 ~dir:Down in
+  let start2 = Pos.create ~r:0 ~c:0 ~moves:0 ~need_moves:0 ~heat_lost:0 ~dir:Right in
   let heap = PosHeap.of_list [start1; start2] in
   let seen = Hashtbl.create (module Int) in
   dijkstras map heap seen (Array.length map - 1, Array.length map.(0) - 1)
