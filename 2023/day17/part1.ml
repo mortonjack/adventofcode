5c80255c01492110d1989f09cdbb9053adbee24f
mortonjack <morton.jack@protonmail.com>
2023-12-17 13:44:40 +0800

    omg functor working??

--- 2023/day17/part1.ml
+++ 2023/day17/part1.ml
@@ -1,6 +1,46 @@
 open! Core
 open Stdio
-open Containers.Heap
+open Containers
+
+type direction = Up | Down | Left | Right
+let new_movecount dir1 dir2 moves =
+  match dir1, dir2 with
+  | Up, Up | Down, Down | Left, Left | Right, Right -> 3
+  | _ -> moves - 1
+
+module Pos = struct
+  type t = {
+    r : int;
+    c : int;
+    moves : int;
+    heat_lost : int;
+    dir : direction
+  }
+  let in_dir pos dir = 
+    let moves = new_movecount dir pos.dir pos.moves in
+    match dir with
+    | Up -> { pos with r = pos.r - 1; moves; dir }
+    | Down -> { pos with r = pos.r + 1; moves; dir }
+    | Left -> { pos with c = pos.c - 1; moves; dir }
+    | Right -> { pos with c = pos.c + 1;  moves; dir }
+
+  let leq first second =
+    first.heat_lost <= second.heat_lost
+
+  let create ~r ~c ~moves ~heat_lost ~dir = {r; c; moves; heat_lost; dir}
+
+end
+
+module PosHeap = Containers.Heap.Make(struct 
+type t = Pos.t 
+let leq = Pos.leq 
+end)
+
+let find_shortest_path map = 
+  let start1 = Pos.create ~r:0 ~c:0 ~moves:0 ~heat_lost:0 ~dir:Down in
+  let start2 = Pos.create ~r:0 ~c:0 ~moves:0 ~heat_lost:0 ~dir:Right in
+  let pq = PosHeap
+
 
 let format_input lines = 
   List.map lines ~f:(fun line 
@@ -13,5 +53,6 @@ let format_input lines =
 let () =
 In_channel.input_lines stdin
 |> format_input
+|> find_shortest_path
 |> Int.to_string
 |> Out_channel.print_endline
