0f8d0bf521513be3ae804777967f38c34551c50c
Jack <104746198+mortonjack@users.noreply.github.com>
2024-12-03 23:11:06 +1030

    add regex + day 3

--- 2024/aoc/src/day03/mod.rs
+++ 2024/aoc/src/day03/mod.rs
@@ -1,7 +1,28 @@
+use regex::Regex;
+
+fn search(input: &str) -> i64 {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    re.captures_iter(input)
+        .map(|s| s.extract())
+        .map(|(_full, [a, b])| a.parse::<i64>().unwrap() * b.parse::<i64>().unwrap())
+        .sum::<i64>()
+}
+
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    format!("Unimplemented. Input length: {}", input.len())
+    search(input)
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
-    format!("Unimplemented. Input length: {}", input.len())
+    input
+        .split_inclusive("do()")
+        .flat_map(|i| i.split_inclusive("don't()"))
+        .fold((true, 0), |(enabled, sum), s| {
+            let disabled = s.ends_with("don't()");
+            if enabled {
+                (!disabled, sum + search(s))
+            } else {
+                (!disabled, sum)
+            }
+        })
+        .1
 }
