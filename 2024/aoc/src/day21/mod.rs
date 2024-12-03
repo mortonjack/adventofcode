dc7e6767532fd01e4e4a309e9ca1d83c1216fc07
Jack <104746198+mortonjack@users.noreply.github.com>
2024-12-03 18:55:59 +1030

    aoc rust setup 2024

---
+++ 2024/aoc/src/day21/mod.rs
@@ -0,0 +1,7 @@
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    format!("Unimplemented. Input length: {}", input.len())
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    format!("Unimplemented. Input length: {}", input.len())
+}
