756a3b334c1030dc50bc6b239729a671415743b7
Jack <104746198+mortonjack@users.noreply.github.com>
2024-12-03 19:40:18 +1030

    day 1 part 1

--- 2024/aoc/src/day01/mod.rs
+++ 2024/aoc/src/day01/mod.rs
@@ -1,5 +1,21 @@
+fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
+        .split('\n')
+        .map(|line| {
+            let mut it = line.split_whitespace().map(|s| s.parse::<u32>().unwrap());
+            (it.next().unwrap(), it.next().unwrap())
+        })
+        .collect()
+}
+
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    format!("Unimplemented. Input length: {}", input.len())
+    let (mut left, mut right) = parse(input);
+    left.sort_unstable();
+    right.sort_unstable();
+    left.into_iter()
+        .zip(right)
+        .map(|(l, r)| u32::abs_diff(l, r))
+        .sum::<u32>()
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
